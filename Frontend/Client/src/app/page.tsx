4200652edda711f3d1b365bd2b3023428d63ccbb
atefe fazeli <atefefazeli1996@outlook.com>
2025-12-24 13:36:56 +0330

    add accordion from shadcn

--- Frontend/Client/src/app/page.tsx
+++ Frontend/Client/src/app/page.tsx
@@ -1,10 +1,25 @@
 import { Button } from "../shared/components/ui/button";
-
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../shared/components/ui/accordion";
 
 export default function Home() {
   return (
     <div className="">
-<Button variant="default" size="sm">click here</Button>
+      <Button variant="default" size="sm">
+        click here
+      </Button>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
